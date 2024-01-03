af10f5a3b0de419fa87ca2dd99fa07f4b0fde546
Woselko <wojciech.wlas@wp.pl>
2024-01-04 00:17:09 +0100

    led + Dht

--- LedBlinkingApp/Program.cs
+++ LedBlinkingApp/Program.cs
@@ -1,2 +1,14 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Device.Gpio;
+
+Console.WriteLine("Blinking LED. Press Ctrl+C to end.");
+const int LED_PIN = 17;
+using var controller = new GpioController();
+controller.OpenPin(LED_PIN, PinMode.Output);
+bool ledOn = true;
+while (true)
+{
+    controller.Write(LED_PIN, ((ledOn) ? PinValue.High : PinValue.Low));
+    Console.WriteLine("LED is {0}", ledOn ? "ON" : "OFF");
+    ledOn = !ledOn;
+    Thread.Sleep(1000);
+}
