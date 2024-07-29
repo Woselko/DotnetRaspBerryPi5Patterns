a2fad14b3c9fc5b5a5d48ffb028729a19dd23dcb
DS\PTFK76 <ptfk76@motorolasoultions.com>
2024-07-30 00:56:36 +0200

    videoDetection

--- MQ9GasSensorApp/Program.cs
+++ MQ9GasSensorApp/Program.cs
@@ -14,7 +14,7 @@ class Program
         };
 
         using var spiDevice = SpiDevice.Create(spiSettings);
-        
+
         // Chip Select (CS) na GPIO 5
         using var gpioController = new GpioController();
         int chipSelectPin = 25;
@@ -24,13 +24,13 @@ class Program
         while (true)
         {
             gpioController.Write(chipSelectPin, PinValue.Low);
-            
+
             // Wysłanie polecenia odczytu dla kanału 0
             byte[] writeBuffer = new byte[3] { 0x18, 0x00, 0x00 }; // 0x18 = 00011000, które wybiera kanał 0
             byte[] readBuffer = new byte[3];
 
             spiDevice.TransferFullDuplex(writeBuffer, readBuffer);
-            
+
             gpioController.Write(chipSelectPin, PinValue.High);
 
             // Przetwarzanie wyniku
@@ -42,3 +42,75 @@ class Program
         }
     }
 }
+
+// using System;
+// using System.Device.Gpio;
+// using System.Device.Spi;
+
+// public class ADC0834
+// {
+//     private SpiDevice _spiDevice;
+//     private GpioController _gpioController;
+//     private int _chipSelectPin;
+
+//     public ADC0834(SpiDevice spiDevice, GpioController gpioController, int chipSelectPin)
+//     {
+//         _spiDevice = spiDevice;
+//         _gpioController = gpioController;
+//         _chipSelectPin = chipSelectPin;
+//         _gpioController.OpenPin(_chipSelectPin, PinMode.Output);
+//         _gpioController.Write(_chipSelectPin, PinValue.High);
+//     }
+
+//     public double ReadChannel(int channel)
+//     {
+//         if (channel < 0 || channel > 3)
+//         {
+//             throw new ArgumentOutOfRangeException(nameof(channel), "Channel must be between 0 and 3.");
+//         }
+
+//         _gpioController.Write(_chipSelectPin, PinValue.Low);
+
+//         byte startBit = 0x80;
+//         byte singleEnded = 0x20;
+//         byte channelBits = (byte)(channel << 4);
+
+//         byte[] writeBuffer = new byte[] { (byte)(startBit | singleEnded | channelBits), 0x00, 0x00 };
+//         byte[] readBuffer = new byte[3];
+
+//         _spiDevice.TransferFullDuplex(writeBuffer, readBuffer);
+
+//         _gpioController.Write(_chipSelectPin, PinValue.High);
+
+//         int result = ((readBuffer[1] & 0x0F) << 8) | readBuffer[2];
+//         return (double)result / 4096.0 * 3.3; // Przeskalowanie wyniku do zakresu 0-3.3V
+//     }
+// }
+
+// class Program
+// {
+//     static void Main(string[] args)
+//     {
+//         var spiConnectionSettings = new SpiConnectionSettings(0, 0)
+//         {
+//             ClockFrequency = 500000,
+//             Mode = SpiMode.Mode0
+//         };
+
+//         var spiDevice = SpiDevice.Create(spiConnectionSettings);
+//         var gpioController = new GpioController();
+
+//         // Chip Select (CS) na GPIO 25 (pin 22)
+//         int chipSelectPin = 25;
+
+//         var adc = new ADC0834(spiDevice, gpioController, chipSelectPin);
+
+//         while (true)
+//         {
+//             double value = adc.ReadChannel(0); // Odczyt z kanału 0
+//             Console.WriteLine($"Odczytana wartość z MQ-9: {value:F2} V");
+
+//             System.Threading.Thread.Sleep(1000);
+//         }
+//     }
+// }
---
+++ VideoObjectDetection/Program.cs
@@ -0,0 +1,77 @@
+﻿using Emgu.CV.Dnn;
+using Emgu.CV.Structure;
+using Emgu.CV.Util;
+using Emgu.CV;
+
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        //yolov3.weights need to be downloaded from https://pjreddie.com/darknet/yolo/ its >200mb
+        var net = Emgu.CV.Dnn.DnnInvoke.ReadNetFromDarknet("yolov3.cfg", "yolov3.weights");
+        var classLabels = File.ReadAllLines("coco.names");
+        net.SetPreferableBackend(Emgu.CV.Dnn.Backend.OpenCV);
+        net.SetPreferableTarget(Emgu.CV.Dnn.Target.Cpu);
+        //var vc = new VideoCapture(0, VideoCapture.API.DShow);
+        string videoPath = "test1.avi";
+        VideoCapture vc = new VideoCapture(videoPath);
+        Mat frame = new();
+        VectorOfMat output = new();
+        VectorOfRect boxes = new();
+        VectorOfFloat scores = new();
+        VectorOfInt indices = new();
+        while (true)
+        {
+            vc.Read(frame);
+            CvInvoke.Resize(frame, frame, new System.Drawing.Size(0, 0), .4, .4);
+            boxes = new();
+            indices = new();
+            scores = new();
+            var image = frame.ToImage<Bgr, byte>();
+            var input = DnnInvoke.BlobFromImage(image, 1 / 255.0, swapRB: true);
+            net.SetInput(input);
+            net.Forward(output, net.UnconnectedOutLayersNames);
+            for (int i = 0; i < output.Size; i++)
+            {
+                var mat = output[i];
+                var data = (float[,])mat.GetData();
+                for (int j = 0; j < data.GetLength(0); j++)
+                {
+                    float[] row = Enumerable.Range(0, data.GetLength(1))
+                                  .Select(x => data[j, x])
+                                  .ToArray();
+                    var rowScore = row.Skip(5).ToArray();
+                    var classId = rowScore.ToList().IndexOf(rowScore.Max());
+                    var confidence = rowScore[classId];
+                    if (confidence > 0.8f)
+                    {
+                        var centerX = (int)(row[0] * frame.Width);
+                        var centerY = (int)(row[1] * frame.Height);
+                        var boxWidth = (int)(row[2] * frame.Width);
+                        var boxHeight = (int)(row[3] * frame.Height);
+                        var x = (int)(centerX - (boxWidth / 2));
+                        var y = (int)(centerY - (boxHeight / 2));
+                        boxes.Push(new System.Drawing.Rectangle[] { new System.Drawing.Rectangle(x, y, boxWidth, boxHeight) });
+                        indices.Push(new int[] { classId });
+                        scores.Push(new float[] { confidence });
+                    }
+                }
+            }
+            var bestIndex = DnnInvoke.NMSBoxes(boxes.ToArray(), scores.ToArray(), .8f, .8f);
+            var frameOut = frame.ToImage<Bgr, byte>();
+            for (int i = 0; i < bestIndex.Length; i++)
+            {
+                int index = bestIndex[i];
+                var box = boxes[index];
+                var text = classLabels[indices[index]];
+                CvInvoke.Rectangle(frameOut, box, new MCvScalar(0, 255, 0), 2);
+                CvInvoke.PutText(frameOut, text, new System.Drawing.Point(box.X, box.Y - 20),
+                Emgu.CV.CvEnum.FontFace.HersheyPlain, 1.0, new MCvScalar(0, 0, 255), 2);
+            }
+            CvInvoke.Resize(frameOut, frameOut, new System.Drawing.Size(0, 0), 4, 4);
+            CvInvoke.Imshow("output", frameOut);
+            if (CvInvoke.WaitKey(1) == 27)
+                break;
+        }
+    }
+}
